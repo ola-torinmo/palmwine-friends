35e0ea374518b352aaab168c4be96ba279b6fff0
torinmo <torinmo.work@gmailcom>
2024-04-11 16:56:50 +0100

    tweaks

--- src/pages/Sign_up.jsx
+++ src/pages/Sign_up.jsx
@@ -46,17 +46,17 @@ function Sign_up() {
     <main
         className={`w-screen`}
       >
-        <section className="h-screen md:h-[1290px] md:bg-contain bg-contain bg-center bg-no-repeat  bg-gray-500 bg-blend-multiply hidden md:block  " style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <section className="h-screen md:h-[1290px] md:bg-contain bg-contain bg-center bg-no-repeat   hidden md:block  " style={{ backgroundImage: `url(${backgroundImage})` }}>
         
           <img src={logo} alt='' className="mx-4  py-4 md:mt-[-1.5%] ml-[3.5%]  w-[90px] h-[70px]"/>
  
         </section>
 
          {/* smaller screens */}
-        <section className="h-screen md:h-[1290px] md:bg-contain bg-contain bg-center bg-no-repeat  bg-gray-500 bg-blend-multiply md:hidden mt-[-30%] se:mt-[90%]  " style={{ backgroundImage: `url(${backgroundImage2})` }}>
+        <section className="h-screen md:h-[1290px] md:bg-contain bg-contain bg-center bg-no-repeat   md:hidden mt-[-30%] se:mt-[0%]  " style={{ backgroundImage: `url(${backgroundImage2})` }}>
         
-          <div className="pt-[54%] ">
-          <img src={logo} alt='' className="  ml-[3.5%]  md:w-[90px] w-[50px] md:h-[70px] h-[30px]"/>
+          <div className="pt-[54%] ip12:pt-[54%] ip14:pt-[54%] ipx:pt-[54%] md:pt-[54%]  ">
+          <img src={logo} alt='' className=" ip12:ml-[3.5%] ip14:ml-[3.5%] ipx:ml-[3.5%] md:ml-[3.5%]  ml-[3.5%]  md:w-[90px] w-[50px] md:h-[70px] h-[30px]"/>
           </div>
  
         </section>
@@ -67,7 +67,7 @@ function Sign_up() {
         <div className="" >
           <div className="md:flex md:mt-[-1.5%] mt-[-40%]">
           {/* <img src="/images/logo.png" className="mx-5 py-5 w-[120px] h-[100px] ml-[5%] "/> */}
-          <img src={star1} alt='' className="md:h-[70px] h-[25px] md:ml-[47%] ml-[47%] md:mt-[10%] pt-[2%] "/>
+          <img src={star1} alt='' className="md:h-[70px] h-[25px]  md:ml-[47%] ml-[47%] md:mt-[10%] pt-[2%] "/>
           
           </div>
 
@@ -81,8 +81,8 @@ function Sign_up() {
           
           </div>
           
-          <div className="md:ml-[70%] ml-[82%] md:mt-[25%] mt-[] ">
-            <img src={line3} alt='' className=" md:w-[120px] md:h-[90px] h-[60px] w-[60px] md:mt-[30%] mt-[40%] se:mt-[0%]  "/>
+          <div className="md:ml-[70%] ml-[82%] md:mt-[25%]  ">
+            <img src={line3} alt='' className=" md:w-[120px] md:h-[90px] h-[60px] w-[60px] md:mt-[30%] mt-[40%] se:mt-[60%]  "/>
           </div>
         </div>
           
@@ -93,7 +93,7 @@ function Sign_up() {
 
               
 
-                <form  onSubmit={submitHandler}  className=" md:mx-[25%] mx-[21%] md:mt-[-25%] mt-[-39%] ip12:mt-[-44%] ip14:mt-[-36%] se:mt-[-70%]" style={{ fontFamily: 'moderat' }}>
+                <form  onSubmit={submitHandler}  className=" md:mx-[25%] mx-[21%] md:mt-[-25%] mt-[-45%] ip12:mt-[-44%] ip14:mt-[-36%] ipx:mt-[-39%] " style={{ fontFamily: 'moderat' }}>
                   <div className="md:mb-[45px] mb-[15px]  relative">
                    
                     <input type="text"
@@ -120,7 +120,7 @@ function Sign_up() {
                       <img src={email} alt='' className="h-5 w-5"/> 
                   </div> 
                   </div>
-                  <div className="md:mb-5 relative">
+                  <div className="md:mb-5 pb-[%] relative">
                     
                     <input 
                     onChange={changeHandler}
@@ -136,8 +136,8 @@ function Sign_up() {
                   
                   
 
-                  <div className="flex flex-col  sm:flex-row sm:justify-center sm:space-y-0 pb-[5%]">
-                    <button className="inline-flex justify-center items-center mt-[8%] se:mt-[-1] md:py-5 py-3 md:w-[450px] w-[200px] border-2 border-black md:text-[32px] text-[18px] font-medium text-center text-black rounded-none bg-[#e7ee06] md:mb-[0%] md:ml-[0%] mb-[1%] ml-[12%]  " type='submit'   style={{ fontFamily: 'PressStart2P' }} >
+                  <div className="flex  sm:justify-center  pb-[5%] ">
+                    <button className="inline-flex justify-center items-center ip12:mt-[8%] ip14:mt-[8%] ipx:mt-[8%] md:mt-[8%] mt-[5%]  md:py-5 py-3 md:w-[450px] w-[200px] border-2 border-black md:text-[32px] text-[18px] font-medium text-center text-black rounded-none bg-[#e7ee06] md:mb-[0%] md:ml-[0%] mb-[1%] ml-[12%]  " type='submit'   style={{ fontFamily: 'PressStart2P' }} >
                         Sign up
                         
                     </button >
